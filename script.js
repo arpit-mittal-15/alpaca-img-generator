c7bf846ebd3331a1d9bbe549bf672f6ff5979401
arpit-mittal-15 <arpitmittaldhn@gmail.com>
2024-05-14 04:27:04 +0530

    upload

---
+++ script.js
@@ -0,0 +1,349 @@
+function addEvent(){
+    document.getElementById("style-background-blue50").addEventListener("click", () => {
+        changePart("alpaca/backgrounds/blue50.png", "background", "blue50")
+    });
+    document.getElementById("style-background-blue60").addEventListener("click", () => {
+        changePart("alpaca/backgrounds/blue60.png", "background", "blue60")
+    });
+    document.getElementById("style-background-blue70").addEventListener("click", () => {
+        changePart("alpaca/backgrounds/blue70.png", "background", "blue70")
+    });
+    document.getElementById("style-background-darkblue30").addEventListener("click", () => {
+        changePart("alpaca/backgrounds/darkblue30.png", "background", "darkblue30")
+    });
+    document.getElementById("style-background-darkblue50").addEventListener("click", () => {
+        changePart("alpaca/backgrounds/darkblue50.png", "background", "darkblue50")
+    });
+    document.getElementById("style-background-darkblue70").addEventListener("click", () => {
+        changePart("alpaca/backgrounds/darkblue70.png", "background", "darkblue70")
+    });
+    document.getElementById("style-background-green50").addEventListener("click", () => {
+        changePart("alpaca/backgrounds/green50.png", "background", "green50")
+    });
+    document.getElementById("style-background-green60").addEventListener("click", () => {
+        changePart("alpaca/backgrounds/green60.png", "background", "green60")
+    });
+    document.getElementById("style-background-green70").addEventListener("click", () => {
+        changePart("alpaca/backgrounds/green70.png", "background", "green70")
+    });
+    document.getElementById("style-background-grey40").addEventListener("click", () => {
+        changePart("alpaca/backgrounds/grey40.png", "background", "grey40")
+    });
+    document.getElementById("style-background-grey70").addEventListener("click", () => {
+        changePart("alpaca/backgrounds/grey70.png", "background", "grey70")
+    });
+    document.getElementById("style-background-grey80").addEventListener("click", () => {
+        changePart("alpaca/backgrounds/grey80.png", "background", "grey80")
+    });
+    document.getElementById("style-background-red50").addEventListener("click", () => {
+        changePart("alpaca/backgrounds/red50.png", "background", "red50")
+    });
+    document.getElementById("style-background-red60").addEventListener("click", () => {
+        changePart("alpaca/backgrounds/red60.png", "background", "red60")
+    });
+    document.getElementById("style-background-red70").addEventListener("click", () => {
+        changePart("alpaca/backgrounds/red70.png", "background", "red70")
+    });
+    document.getElementById("style-background-yellow50").addEventListener("click", () => {
+        changePart("alpaca/backgrounds/yellow50.png", "background", "yellow50")
+    });
+    document.getElementById("style-background-yellow60").addEventListener("click", () => {
+        changePart("alpaca/backgrounds/yellow60.png", "background", "yellow60")
+    });
+    document.getElementById("style-background-yellow70").addEventListener("click", () => {
+        changePart("alpaca/backgrounds/yellow70.png", "background", "yellow70")
+    });
+
+
+    document.getElementById("style-neck-thick").addEventListener("click", () => {
+        changePart("alpaca/neck/thick.png", "neck", "thick")
+    });
+    document.getElementById("style-neck-backward").addEventListener("click", () => {
+        changePart("alpaca/neck/bend-backward.png", "neck", "backward")
+    });
+    document.getElementById("style-neck-forward").addEventListener("click", () => {
+        changePart("alpaca/neck/bend-forward.png", "neck", "forward")
+    });
+    document.getElementById("style-neck-default").addEventListener("click", () => {
+        changePart("alpaca/neck/default.png", "neck", "default")
+    });
+
+
+    document.getElementById("style-ears-default").addEventListener("click", () => {
+        changePart("alpaca/ears/default.png", "ears", "default")
+    });
+    document.getElementById("style-ears-backward").addEventListener("click", () => {
+        changePart("alpaca/ears/tilt-backward.png", "ears", "backward")
+    });
+    document.getElementById("style-ears-forward").addEventListener("click", () => {
+        changePart("alpaca/ears/tilt-forward.png", "ears", "forward")
+    });
+
+
+    document.getElementById("style-hair-default").addEventListener("click", () => {
+        changePart("alpaca/hair/default.png", "hair", "default")
+    });
+    document.getElementById("style-hair-bang").addEventListener("click", () => {
+        changePart("alpaca/hair/bang.png", "hair", "bang")
+    });
+    document.getElementById("style-hair-curls").addEventListener("click", () => {
+        changePart("alpaca/hair/curls.png", "hair", "curls")
+    });
+    document.getElementById("style-hair-elegant").addEventListener("click", () => {
+        changePart("alpaca/hair/elegant.png", "hair", "elegant")
+    });
+    document.getElementById("style-hair-quiff").addEventListener("click", () => {
+        changePart("alpaca/hair/quiff.png", "hair", "quiff")
+    });
+    document.getElementById("style-hair-short").addEventListener("click", () => {
+        changePart("alpaca/hair/short.png", "hair", "short")
+    });
+
+
+    document.getElementById("style-eyes-default").addEventListener("click", () => {
+        changePart("alpaca/eyes/default.png", "eyes", "default")
+    });
+    document.getElementById("style-eyes-angry").addEventListener("click", () => {
+        changePart("alpaca/eyes/angry.png", "eyes", "angry")
+    });
+    document.getElementById("style-eyes-naughty").addEventListener("click", () => {
+        changePart("alpaca/eyes/naughty.png", "eyes", "naughty")
+    });
+    document.getElementById("style-eyes-panda").addEventListener("click", () => {
+        changePart("alpaca/eyes/panda.png", "eyes", "panda")
+    });
+    document.getElementById("style-eyes-smart").addEventListener("click", () => {
+        changePart("alpaca/eyes/smart.png", "eyes", "smart")
+    });
+    document.getElementById("style-eyes-star").addEventListener("click", () => {
+        changePart("alpaca/eyes/star.png", "eyes", "star")
+    });
+
+
+    document.getElementById("style-mouth-default").addEventListener("click", () => {
+        changePart("alpaca/mouth/default.png", "mouth", "default")
+    });
+    document.getElementById("style-mouth-astonished").addEventListener("click", () => {
+        changePart("alpaca/mouth/astonished.png", "mouth", "astonished")
+    });
+    document.getElementById("style-mouth-eating").addEventListener("click", () => {
+        changePart("alpaca/mouth/eating.png", "mouth", "eating")
+    });
+    document.getElementById("style-mouth-laugh").addEventListener("click", () => {
+        changePart("alpaca/mouth/laugh.png", "mouth", "laugh")
+    });
+    document.getElementById("style-mouth-tongue").addEventListener("click", () => {
+        changePart("alpaca/mouth/tongue.png", "mouth", "tongue")
+    });
+
+
+    document.getElementById("style-leg-default").addEventListener("click", () => {
+        changePart("alpaca/leg/default.png", "leg", "default")
+    });
+    document.getElementById("style-leg-bubble").addEventListener("click", () => {
+        changePart("alpaca/leg/bubble-tea.png", "leg", "bubble")
+    });
+    document.getElementById("style-leg-cookie").addEventListener("click", () => {
+        changePart("alpaca/leg/cookie.png", "leg", "cookie")
+    });
+    document.getElementById("style-leg-game").addEventListener("click", () => {
+        changePart("alpaca/leg/game-console.png", "leg", "game")
+    });
+    document.getElementById("style-leg-backward").addEventListener("click", () => {
+        changePart("alpaca/leg/tilt-backward.png", "leg", "backward")
+    });
+    document.getElementById("style-leg-forward").addEventListener("click", () => {
+        changePart("alpaca/leg/tilt-forward.png", "leg", "forward")
+    });
+
+
+    document.getElementById("style-accessory-headphone").addEventListener("click", () => {
+        changePart("alpaca/accessories/headphone.png", "accessory", "headphone")
+    });
+    document.getElementById("style-accessory-earings").addEventListener("click", () => {
+        changePart("alpaca/accessories/earings.png", "accessory", "earings")
+    });
+    document.getElementById("style-accessory-flower").addEventListener("click", () => {
+        changePart("alpaca/accessories/flower.png", "accessory", "flower")
+    });
+    document.getElementById("style-accessory-glasses").addEventListener("click", () => {
+        changePart("alpaca/accessories/glasses.png", "accessory", "glasses")
+    });
+
+
+    document.getElementById("accessory-hair").addEventListener("click", () => {
+        selectPart("hair")
+    });
+    document.getElementById("accessory-ears").addEventListener("click", () => {
+        selectPart("ears")
+    });
+    document.getElementById("accessory-eyes").addEventListener("click", () => {
+        selectPart("eyes")
+    });
+    document.getElementById("accessory-mouth").addEventListener("click", () => {
+        selectPart("mouth")
+    });
+    document.getElementById("accessory-neck").addEventListener("click", () => {
+        selectPart("neck")
+    });
+    document.getElementById("accessory-leg").addEventListener("click", () => {
+        selectPart("leg")
+    });
+    document.getElementById("accessory-accessory").addEventListener("click", () => {
+        selectPart("accessory")
+    });
+    document.getElementById("accessory-background").addEventListener("click", () => {
+        selectPart("background")
+    });
+
+}
+
+function selectPart(part){
+    document.getElementsByClassName("active")[0].classList.remove("active");
+    document.getElementById("accessory-"+part).classList.add("active");
+    document.getElementsByClassName("displaying")[0].classList.remove("displaying");
+    document.getElementById("style-"+part).classList.add("displaying");
+}
+
+function changePart(source, bodypart, styletype){
+    let part_id = 100;
+    switch(bodypart){
+        case "background":
+            part_id = 1;
+            break;
+        case "neck":
+            part_id = 2;
+            break;
+        case "ears":
+            part_id = 3;
+            break;
+        case "hair":
+            part_id = 4;
+            break;
+        case "eyes":
+            part_id = 5;
+            break;
+        case "mouth":
+            part_id = 6;
+            break;
+        case "leg":
+            part_id = 7;
+            break;
+        case "accessory":
+            part_id = 8;
+    }
+    document.getElementsByClassName("active")[part_id].classList.remove("active")
+    
+    var elem = document.createElement("img");
+    elem.setAttribute("src", source);
+    elem.classList.add("part")
+    elem.classList.add("alpaca-" + bodypart)
+    elem.classList.add("visible")
+    elem.id = "alpaca-" + bodypart + "-" + styletype ;
+
+    let oldelem = document.getElementsByClassName("alpaca-" + bodypart);
+    let parent = document.getElementById("alpaca-parts");
+
+    parent.replaceChild(elem, oldelem[0]);
+
+    setTimeout(document.getElementById("style-"+bodypart+"-"+styletype).classList.add("active"),0)
+}
+function download(){
+    html2canvas(document.getElementById('alpaca-container')).then(function(canvas) {
+        let canvasUrl = canvas.toDataURL();
+        const createEl = document.createElement('a');
+        createEl.href = canvasUrl;
+        createEl.download = "your-alpaca";
+        createEl.click();
+        createEl.remove();
+    });
+};
+function random(){
+    
+    let bgsource = [
+        ["alpaca/backgrounds/blue50.png","blue50"],
+        ["alpaca/backgrounds/blue60.png","blue60"],
+        ["alpaca/backgrounds/blue70.png","blue70"],
+        ["alpaca/backgrounds/darkblue30.png","darkblue30"],
+        ["alpaca/backgrounds/darkblue50.png","darkblue50"],
+        ["alpaca/backgrounds/darkblue70.png","darkblue70"],
+        ["alpaca/backgrounds/green50.png","green50"],
+        ["alpaca/backgrounds/green60.png","green60"],
+        ["alpaca/backgrounds/green70.png","green70"],
+        ["alpaca/backgrounds/grey40.png","grey40"],
+        ["alpaca/backgrounds/grey70.png","grey70"],
+        ["alpaca/backgrounds/grey80.png","grey80"],
+        ["alpaca/backgrounds/red50.png","red50"],
+        ["alpaca/backgrounds/red60.png","red60"],
+        ["alpaca/backgrounds/red70.png","red70"],
+        ["alpaca/backgrounds/yellow50.png","yellow50"],
+        ["alpaca/backgrounds/yellow60.png","yellow60"],
+        ["alpaca/backgrounds/yellow70.png","yellow70"],
+    ];
+    let accsource = [
+        ["alpaca/accessories/earings.png","earings"],
+        ["alpaca/accessories/flower.png","flower"],
+        ["alpaca/accessories/glasses.png","glasses"],
+        ["alpaca/accessories/headphone.png","headphone"],
+    ];
+    let earsource = [
+        ["alpaca/ears/default.png", "default"],
+        ["alpaca/ears/tilt-backward.png", "backward"],
+        ["alpaca/ears/tilt-forward.png", "forward"],
+    ];
+    let eyesource = [
+        ["alpaca/eyes/angry.png", "angry"],
+        ["alpaca/eyes/default.png", "default"],
+        ["alpaca/eyes/naughty.png", "naughty"],
+        ["alpaca/eyes/panda.png", "panda"],
+        ["alpaca/eyes/smart.png", "smart"],
+        ["alpaca/eyes/star.png", "star"],
+    ];
+    let hairsource = [
+        ["alpaca/hair/bang.png", "bang"],
+        ["alpaca/hair/curls.png", "curls"],
+        ["alpaca/hair/default.png", "default"],
+        ["alpaca/hair/elegant.png", "elegant"],
+        ["alpaca/hair/quiff.png", "quiff"],
+        ["alpaca/hair/short.png", "short"],
+    ];
+    let legsource = [
+        ["alpaca/leg/bubble-tea.png", "bubble"],
+        ["alpaca/leg/cookie.png", "cookie"],
+        ["alpaca/leg/default.png", "default"],
+        ["alpaca/leg/game-console.png", "game"],
+        ["alpaca/leg/tilt-backward.png", "backward"],
+        ["alpaca/leg/tilt-forward.png", "forward"],
+    ];
+    let mouthsource = [
+        ["alpaca/mouth/astonished.png", "astonished"],
+        ["alpaca/mouth/default.png", "default"],
+        ["alpaca/mouth/eating.png", "eating"],
+        ["alpaca/mouth/laugh.png", "laugh"],
+        ["alpaca/mouth/tongue.png", "tongue"],
+    ];
+    let necksource = [
+        ["alpaca/neck/bend-backward.png", "backward"],
+        ["alpaca/neck/bend-forward.png", "forward"],
+        ["alpaca/neck/default.png", "default"],
+        ["alpaca/neck/thick.png", "thick"],
+    ];
+
+    let bg = Math.floor(Math.random() * 18);
+    let acc = Math.floor(Math.random() * 4);
+    let ear = Math.floor(Math.random() * 3);
+    let eye = Math.floor(Math.random() * 6);
+    let hair = Math.floor(Math.random() * 6);
+    let leg = Math.floor(Math.random() * 6);
+    let mouths = Math.floor(Math.random() * 5);
+    let neck = Math.floor(Math.random() * 4);
+
+    changePart(bgsource[bg][0], "background", bgsource[bg][1]);
+    changePart(accsource[acc][0], "accessory", accsource[acc][1]);
+    changePart(earsource[ear][0], "ears", earsource[ear][1]);
+    changePart(eyesource[eye][0], "eyes", eyesource[eye][1]);
+    changePart(hairsource[hair][0], "hair", hairsource[hair][1]);
+    changePart(legsource[leg][0], "leg", legsource[leg][1]);
+    changePart(mouthsource[mouths][0], "mouth", mouthsource[mouths][1]);
+    changePart(necksource[neck][0], "neck", necksource[neck][1]);
+}
